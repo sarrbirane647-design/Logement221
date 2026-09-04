880e598f0b4e06add355dd982d9636ab67be9fa7
Birane Sarr <sarrbirane647@gmail.com>
2026-09-04 18:52:15 +0000

    Ajout Firebase App Check reCAPTCHA Enterprise

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import {
+  initializeAppCheck,
+  ReCaptchaEnterpriseProvider
+} from "firebase/app-check";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBc5j5pMxUU_43a6pH0wARKiE_aG_XUtfE",
+  apiKey: "TA_VRAIE_API_KEY",
   authDomain: "logement221-53840.firebaseapp.com",
   projectId: "logement221-53840",
   storageBucket: "logement221-53840.firebasestorage.app",
@@ -12,6 +16,11 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaEnterpriseProvider("6Lfym6ktAAAAANxVijCR8LpZ6-_pBpobWg85uBGv"),
+  isTokenAutoRefreshEnabled: true
+});
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
